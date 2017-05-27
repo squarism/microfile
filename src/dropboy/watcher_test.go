ebc2d4f717d0063e5f9c386f2b507d53f331395e
Chris Dillon <squarism@gmail.com>
2017-05-26 17:23:31 -0700

    minor: change watch test example

--- src/dropboy/watcher_test.go
+++ src/dropboy/watcher_test.go
@@ -13,15 +13,18 @@ func TestRegisterNothing(t *testing.T) {
 
 func TestRegisterWatch(t *testing.T) {
 	watcher := Watcher{}
-	watcher.Register("/tmp", []string{"http://localhost:3000/tmp"})
+	watcher.Register("/resumes", []string{"http://localhost:3000/resumes"})
 
 	assert.Equal(t, len(watcher.Watches), 1)
 }
 
 func TestRegisterWatches(t *testing.T) {
 	watcher := Watcher{}
-	watcher.Register("/tmp", []string{"http://localhost:3000/tmp"})
-	watcher.Register("/var/tmp", []string{"http://localhost:3000/var"})
+	watcher.Register("/real_estate", []string{"http://localhost:3000/image_shrinker"})
+	watcher.Register("/music", []string{
+		"http://localhost:3000/copyright_alerter",
+		"http://localhost:3000/recompressor",
+	})
 
 	assert.Equal(t, len(watcher.Watches), 2)
 }
