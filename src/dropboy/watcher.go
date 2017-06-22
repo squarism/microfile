46bce9533de0cdce01906b1822aacb74e80e8443
Chris Dillon <squarism@gmail.com>
2017-06-22 12:57:06 -0700

    Registering watches

--- src/dropboy/watcher.go
+++ src/dropboy/watcher.go
@@ -5,26 +5,58 @@ import (
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
+
+	"dropboy/config"
 )
 
-type Watcher struct {
-	Watches map[string][]string
+// TODO: rename this whole thing to Dropboy -- it's really the top level thing
+
+type watcher struct {
+	Watches  map[string][]string
+	Notifier *fsnotify.Watcher
 }
 
-func (w *Watcher) Register(watcher *fsnotify.Watcher, path string, urls []string) {
+func NewWatcher() watcher {
+	notifier, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := watcher{}
+	w.Notifier = notifier
+	return w
+}
+
+func (w *watcher) Stop() {
+	w.Notifier.Close()
+}
+
+func (w *watcher) Register(path string, actions []string) {
 	if w.Watches == nil {
 		w.Watches = make(map[string][]string)
 	}
 
-	w.Watches[path] = urls
+	// TODO: I don't know what this is really doing for us here
+	// a list of paths for later use?  The filesystem watcher is stateless.
+	w.Watches[path] = actions
 
 	absPath, err := filepath.Abs(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = watcher.Add(absPath)
+	err = w.Notifier.Add(absPath)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func (w *watcher) RegisterWatchesFromConfig(config config.Config) {
+	for _, watch := range config.Watches {
+		actions := []string{}
+		for _, action := range watch.Actions {
+			actions = append(actions, action.Type)
+		}
+		w.Register(watch.Path, actions)
 	}
 }
--- src/dropboy/watcher_test.go
+++ src/dropboy/watcher_test.go
@@ -1,39 +1,84 @@
 package dropboy
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"testing"
 
-	"github.com/fsnotify/fsnotify"
 	"github.com/stretchr/testify/assert"
+
+	"dropboy/config"
 )
 
-func TestRegisterNothing(t *testing.T) {
-	watcher := Watcher{}
+var directoryToWatch string = "./../../fixtures/dropbox"
 
-	assert.Equal(t, len(watcher.Watches), 0)
-}
-
-func TestRegisterWatch(t *testing.T) {
-	fsnotifyWatcher, err := fsnotify.NewWatcher()
+// We have to duplicate this testing helper method from another test file.
+// Shared test code in Go is either duplicated or documented from godoc.  Which do I want?
+func fixturesDirectory() string {
+	pwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer fsnotifyWatcher.Close()
 
-	watcher := Watcher{}
-	watcher.Register(fsnotifyWatcher, "./../../fixtures/dropbox", []string{"http://localhost:3000/resumes"})
-
-	assert.Equal(t, len(watcher.Watches), 1)
+	return fmt.Sprintf("%s/../../fixtures", pwd)
 }
 
-// func TestRegisterWatches(t *testing.T) {
-// 	watcher := Watcher{}
-// 	watcher.Register("/real_estate", []string{"http://localhost:3000/image_shrinker"})
-// 	watcher.Register("/music", []string{
-// 		"http://localhost:3000/copyright_alerter",
-// 		"http://localhost:3000/recompressor",
-// 	})
+func TestRegisterNothing(t *testing.T) {
+	watcher := NewWatcher()
+	defer watcher.Stop()
 
-// 	assert.Equal(t, len(watcher.Watches), 2)
-// }
+	assert.Equal(t, 0, len(watcher.Watches))
+}
+
+func TestRegisterWatch(t *testing.T) {
+	watcher := NewWatcher()
+	defer watcher.Stop()
+
+	watcher.Register(directoryToWatch, []string{"http://localhost:3000/resumes"})
+
+	assert.Equal(t, 1, len(watcher.Watches))
+}
+
+func TestRegisterWatches(t *testing.T) {
+	realEstateDirectory := fmt.Sprint(directoryToWatch, "/real_estate")
+	musicDirectory := fmt.Sprint(directoryToWatch, "/music")
+
+	watcher := NewWatcher()
+	defer watcher.Stop()
+
+	watcher.Register(realEstateDirectory, []string{"http://localhost:3000/image_shrinker"})
+	watcher.Register(musicDirectory, []string{
+		"http://localhost:3000/copyright_alerter",
+		"http://localhost:3000/recompressor",
+	})
+
+	assert.Equal(t, 2, len(watcher.Watches))
+}
+
+func TestRegisterFromConfig(t *testing.T) {
+	publicFixturesPath := fmt.Sprintf("%s/dropbox", fixturesDirectory())
+
+	var config = config.Config{
+		Watches: []config.Watch{
+			{
+				Path: publicFixturesPath,
+				Actions: []config.Action{
+					{
+						Type:    "http",
+						Options: map[string]string{"path": "/remote/server/path"},
+					},
+				},
+			},
+		},
+	}
+
+	watcher := NewWatcher()
+	defer watcher.Stop()
+	expected := map[string][]string{
+		publicFixturesPath: []string{"http"},
+	}
+
+	watcher.RegisterWatchesFromConfig(config)
+	assert.Equal(t, expected, watcher.Watches)
+}
