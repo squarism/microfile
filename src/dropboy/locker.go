a4dd2e9d7c6394a30693a8bb16a06cb252a1a575
Chris Dillon <squarism@gmail.com>
2017-07-27 16:18:04 -0700

    full locking test and cleanup

--- src/dropboy/locker.go
+++ src/dropboy/locker.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"os"
 	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
@@ -13,14 +14,15 @@ import (
 type locker struct {
 	AllActive     map[string]string
 	AppFs         afero.Fs
-	WorkDirectory string
+	WorkDirectory string // TODO: this should be able to be overridden in the config
 }
 
-func NewLocker(workDir string) locker {
+func NewLocker() locker {
 	l := locker{}
 	l.AllActive = make(map[string]string)
 	l.AppFs = afero.NewOsFs()
-	l.WorkDirectory = workDir
+	l.WorkDirectory = filepath.Join(os.TempDir(), "dropboy")
+
 	return l
 }
 
@@ -30,16 +32,28 @@ func (l *locker) Hash(filename string) string {
 	return fmt.Sprintf("%x", hasher.Sum(nil))
 }
 
-func (l *locker) Lock(filename string, hash string) {
+func (l *locker) Lock(filename string) {
 	if _, ok := l.AllActive[filename]; ok {
-		log.WithFields(log.Fields{"workDir": l.WorkDirectory, "filename": filename}).Info("Already processing")
-	} else {
-		l.AllActive[filename] = hash
+		log.WithFields(log.Fields{
+			"workDir":  l.WorkDirectory,
+			"filename": filename},
+		).Info("Already processing")
+		return
 	}
 
-	l.ensureWorkDirectory(l.WorkDirectory)
+	hash := l.Hash(filename)
+	l.AllActive[filename] = hash
 
-	l.AppFs.Rename(filename, l.hashFilename(hash))
+	l.ensureWorkDirectory(l.WorkDirectory)
+	hashFilename := l.hashFilename(hash)
+
+	err := l.AppFs.Rename(filename, hashFilename)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"workDir":  l.WorkDirectory,
+			"filename": filename},
+		).Fatal("Major problem creating lock file")
+	}
 }
 
 func (l *locker) ensureWorkDirectory(path string) {
--- src/dropboy/locker_test.go
+++ src/dropboy/locker_test.go
@@ -1,17 +1,17 @@
 package dropboy
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
-var workDir string = "/var/dropboy"
+var filename string = "/var/www/uploads/song.mp3"
 
 func TestHashFile(t *testing.T) {
-	locker := NewLocker(workDir)
-	filename := "/var/www/uploads/song.mp3"
+	locker := NewLocker()
 
 	result := locker.Hash(filename)
 
@@ -20,32 +20,51 @@ func TestHashFile(t *testing.T) {
 }
 
 func TestLockFilePushesToList(t *testing.T) {
-	locker := NewLocker(workDir)
-	filename := "/var/www/uploads/song.mp3"
-	hash := "733388170efcc73067c22d5e98c55008"
+	locker := NewLocker()
+	locker.AppFs = afero.NewMemMapFs()
+	locker.AppFs.MkdirAll(filepath.Base(filename), 0755)
+	locker.AppFs.Create(filename)
 
-	locker.Lock(filename, hash)
+	locker.Lock(filename)
 
 	assert.Len(t, locker.AllActive, 1)
 }
 
 func TestEnsureWorkDirectory(t *testing.T) {
-	locker := NewLocker(workDir)
+	locker := NewLocker()
+	workDir := "/1/2/3"
 	locker.AppFs = afero.NewMemMapFs() // for testing
 
 	locker.ensureWorkDirectory(workDir)
-	result, _ := afero.Exists(locker.AppFs, workDir)
+	result, _ := afero.DirExists(locker.AppFs, workDir)
 
 	expected := true
 	assert.Equal(t, expected, result)
 }
 
 func TestHashFileName(t *testing.T) {
-	locker := NewLocker(workDir)
+	locker := NewLocker()
+	locker.WorkDirectory = "/var/dropboy"
 	hash := "733388170efcc73067c22d5e98c55008"
 
 	result := locker.hashFilename(hash)
 
 	expected := "/var/dropboy/733388170efcc73067c22d5e98c55008"
+	assert.Equal(t, expected, result)
+}
+
+func TestLocking(t *testing.T) {
+	locker := NewLocker()
+	locker.WorkDirectory = "/var/dropboy"
+
+	locker.AppFs = afero.NewMemMapFs()
+	locker.AppFs.MkdirAll(filepath.Base(filename), 0755)
+	locker.AppFs.Create(filename)
+
+	locker.Lock(filename)
+	expectedLockedFile := "/var/dropboy/733388170efcc73067c22d5e98c55008"
+
+	result, _ := afero.Exists(locker.AppFs, expectedLockedFile)
+	expected := true
 	assert.Equal(t, expected, result)
 }
